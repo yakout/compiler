c120d17278bc8024d7f700c1c4d788a6804bc11e
Ahmed El-Naggar <ahmednagar2011@gmail.com>
2018-03-16 23:09:55 +0200

    added some code skeleton and header files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+string CALL_ERROR = "Invalid Number of Arguments";
+
+int main(int argc, char** argv) {
+    if (argc == 1)
+    {
+
+    }
+    else if (argc == 2)
+    {
+
+    }
+    else
+    {
+      cout << CALL_ERROR << endl;
+    }
     return 0;
 }
