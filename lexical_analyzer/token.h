c2f651a0aa29b935eff2cc5af3505fdb0dc879c7
MohamedAshrafTolba <tolba96mohamed@gmail.com>
2018-03-21 17:27:25 +0200

    Modified CMakeLists.txt and updated .gitignore.

--- lexical_analyzer/token.h
+++ lexical_analyzer/token.h
@@ -1,14 +1,15 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
+#include <string>
 #include <vector>
 
-typedef token_t string;
+typedef std::string token_t;
 
 struct token
 {
 	token_t type;
-	std::vector<string> lexem; // token can be associated with one lexeme (e.g '<') or multiple lexems (e.g 123725).
+	std::vector<std::string> lexem; // token can be associated with one lexeme (e.g '<') or multiple lexems (e.g 123725).
 	std::string attr_value; // need more info
 	//position_t pos; // position in source program, will help in error report
 };
