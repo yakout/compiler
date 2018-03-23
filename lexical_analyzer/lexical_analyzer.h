7e9a9fe5b9ad7ce615f7bceffc146315a7159067
MohamedAshrafTolba <tolba96mohamed@gmail.com>
2018-03-23 14:59:19 +0200

    Modified acceptance states parsing from transition table.

--- lexical_analyzer/lexical_analyzer.h
+++ lexical_analyzer/lexical_analyzer.h
@@ -7,14 +7,20 @@
 #include <string>
 #include <vector>
 
+
+struct acceptance_state {
+  int state_id;
+  std::string token_class;
+};
+
 class lexical_analyzer {
 private:
   std::string lexical_analyzer_file;
   std::string code_file;
   int start_state_id;
-  std::vector<int> acceptance_states_ids;
   int total_states;
   std::shared_ptr<dfa> dfa_ptr;
+  std::vector<acceptance_state> acceptance_states_info;
   std::vector<std::string> transition_table_inputs;
   std::shared_ptr<dfa> parse_lexical_analyzer_machine ();
 public:
