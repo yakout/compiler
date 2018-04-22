d5842d410569a1c588c9a79a1c993b1c1468ed5b
MohamedAshrafTolba <tolba96mohamed@gmail.com>
2018-04-22 02:48:43 +0200

    Added some bash scripts to automate the process of clean build.

---
+++ lexical_analyzer/lexical_analyzer_runner.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Checks if "build" directory exists or not. If it exists then it will 
+# not create another one to avoid unnecessary warning messages else it will create one.
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+cd build/
+
+# Checks if "CMakeCache.txt" file exists or not. If it exists then it will remove it
+# to avoid confusion between multiple building modes.
+#if [ -f "CMakeCache.txt" ]; then
+#    rm CMakeCache.txt
+#fi
+
+# Running cmake on the CMakeLists.txt in the "lexical_analyzer" (parent) directory.
+
+# 1- With the option of building unit tests turned ON.
+#cmake -DBUILD_UNIT_TEST=ON ..
+
+# 2- With the option of building unit tests turned OFF explicitly.
+cmake -DBUILD_UNIT_TEST=ON ..
+
+# 3- With the option of building unit tests turned OFF implicitly.
+#    NOTING that this option will work correctly as long as we remove CMakeCache.txt
+#    everytime because if we build this module with -DBUILD_UNIT_TEST=ON this value will be
+#    cached and if we didn't turn this option off explicity using -DBUILD_UNIT_TEST=OFF
+#    cmake will use the cached value and will build the tests accordingly even if we don't
+#    need them in our build.
+#cmake ..
+
+make
+
+# "ctest" or "make test" can be used and both will work correctly as long as we remove
+# CMakeCache.txt but "make test" will work correctly either we remove it or not as it
+# doesn't use cached test results (Dangerous! maybe misleading) as ctest; It re-builds 
+# modified tests individually so their results are always up-to-date.
+make test
+
+#ctest
