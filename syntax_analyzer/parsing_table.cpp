af627df9dae586856df21828687e30880e9365f2
Ahmed El-Naggar <ahmednagar2011@gmail.com>
2018-04-25 21:49:20 +0200

    Adjusted the parsing table building implementation to fit the new impelementation of the cfg_set.h and cfg.h

--- syntax_analyzer/parsing_table.cpp
+++ syntax_analyzer/parsing_table.cpp
@@ -28,52 +28,58 @@ std::shared_ptr<cfg_production> get_synch_prod ()
 
 void parsing_table::build()
 {
-//    /// list of non-terminals in the CFG.
-//    std::vector <cfg_symbol> non_terminals = grammar.get_non_terminals ();
-//    /// First and follow cfg_sets
-//    std::shared_ptr <cfg_set> first_cfg_set = grammar.get_first_set();
-//    std::shared_ptr <cfg_set> follow_cfg_set = grammar.get_follow_set();
-//    /// First and Follow maps.
-//    std::unordered_map<std::string, std::vector<cfg_symbol>> first_set =
-//              first_cfg_set->get_set_map();
-//    std::unordered_map<std::string, std::vector<cfg_symbol>> follow_set =
-//              follow_cfg_set->get_set_map();
-//    for (auto non_terminal : non_terminals)
-//    {
-//        std::vector <cfg_symbol> first = first_set[non_terminal.get_name()];
-//        std::vector <cfg_symbol> follow = follow_set[non_terminal.get_name()];
-//        bool has_eps = false;
-//        cfg_symbol eps_terminal;
-//        // filling table with first symbols except for EPS case.
-//        for (auto first_terminal : first)
-//        {
-//            if (first_terminal.get_name() != EPS)
-//                table[make_pair(non_terminal.get_name(),first_terminal.get_name())]
-//                      = *first_terminal.get_parent_prod();
-//            else
-//            {
-//                eps_terminal = first_terminal;
-//                has_eps = true;
-//            }
-//        }
-//        if (has_eps)
-//        {
-//            for (auto follow_terminal : follow)
-//            {
-//                table[make_pair(non_terminal.get_name(), follow_terminal.get_name())]
-//                      = *eps_terminal.get_parent_prod();
-//            }
-//        }
-//        else
-//        {
-//          cfg_production synch_prod = *get_synch_prod();
-//          for (auto follow_terminal : follow)
-//          {
-//              table[make_pair(non_terminal.get_name(), follow_terminal.get_name())]
-//                    = synch_prod;
-//          }
-//        }
-//    }
+   /// list of non-terminals in the CFG.
+   std::vector <cfg_symbol> non_terminals = grammar.get_non_terminals ();
+   /// First and follow cfg_sets
+   std::shared_ptr <cfg_set> first_cfg_set = grammar.get_first_set();
+   std::shared_ptr <cfg_set> follow_cfg_set = grammar.get_follow_set();
+   /// First and Follow maps.
+   std::unordered_map<std::string, std::vector<std::pair<cfg_symbol,
+                      std::shared_ptr<cfg_production>>>> first_set =
+             first_cfg_set->get_set_map();
+   std::unordered_map<std::string, std::vector<std::pair<cfg_symbol,
+                      std::shared_ptr<cfg_production>>>> follow_set =
+             follow_cfg_set->get_set_map();
+   for (auto non_terminal : non_terminals)
+   {
+      std::vector<std::pair<cfg_symbol,
+                  std::shared_ptr<cfg_production>>> first = first_set[non_terminal.get_name()];
+      std::vector<std::pair<cfg_symbol,
+                  std::shared_ptr<cfg_production>>> follow = follow_set[non_terminal.get_name()];
+       bool has_eps = false;
+       cfg_symbol eps_terminal;
+       std::shared_ptr <cfg_production> eps_prod;
+       // filling table with first symbols except for EPS case.
+       for (auto first_terminal : first)
+       {
+           if (first_terminal.first.get_name() != EPS)
+               table[make_pair(non_terminal.get_name(), first_terminal.first.get_name())]
+                     = *first_terminal.second;
+           else
+           {
+               eps_terminal = first_terminal.first;
+               eps_prod = first_terminal.second;
+               has_eps = true;
+           }
+       }
+       if (has_eps)       /// case of eps production in the rule.
+       {
+           for (auto follow_terminal : follow) /// add eps production to follow entries in table
+           {
+               table[make_pair(non_terminal.get_name(), follow_terminal.first.get_name())]
+                     = *eps_prod;
+           }
+       }
+       else
+       {
+         cfg_production synch_prod = *get_synch_prod();
+         for (auto follow_terminal : follow)
+         {
+             table[make_pair(non_terminal.get_name(), follow_terminal.first.get_name())]
+                   = synch_prod;
+         }
+       }
+   }
 }
 
 cfg_production parsing_table::get_production (std::string rule, std::string token)
