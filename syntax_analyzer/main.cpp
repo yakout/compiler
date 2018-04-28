bd717703302f37e242cd2aac2128ca26d07b4962
Ahmed Walid <ahmed3walid96@gmail.com>
2018-04-28 09:55:48 +0200

    Added 2 test fiiles.

--- syntax_analyzer/main.cpp
+++ syntax_analyzer/main.cpp
@@ -26,74 +26,75 @@ int main (int argc, char *argv[]) {
 
 //    cfg cfg_ob = cfg ("../../tests/syntax_analyzer/unit/ready_ll1_cfg.bnf");
 //    cfg cfg_ob = cfg ("../../tests/syntax_analyzer/unit/cfg_single_line_ll1.bnf");
-    cfg cfg_ob = cfg ("../tests/syntax_analyzer/unit/ps_cfg_single_line.bnf");
+//    cfg cfg_ob = cfg ("../tests/syntax_analyzer/unit/ps_cfg_single_line.bnf");
+//    cfg cfg_ob = cfg ("../tests/syntax_analyzer/unit/left_rec_left_fact.bnf");
+    cfg cfg_ob = cfg ("../tests/syntax_analyzer/unit/first_follow_test.bnf");
     std::unordered_map<cfg_symbol, cfg_rule, cfg_symbol::hasher, cfg_symbol::comparator> grammar;
     /** Grammar Checking. **/
     grammar = cfg_ob.get_grammar ();
 
-    cfg_ob.left_factor();
-    cfg_ob.remove_left_recursion();
+//    cfg_ob.left_factor();
+//    cfg_ob.remove_left_recursion();
 
-    std::cout << "+++++++++" << std::endl;
-    for (auto g : cfg_ob.get_grammar())
-    {
-        std::cout << g.second.to_string() << std::endl;
-    }
-    std::cout << "+++++++++" << std::endl;
+//    std::cout << "+++++++++" << std::endl;
+//    for (auto g : cfg_ob.get_grammar())
+//    {
+//        std::cout << g.second.to_string() << std::endl;
+//    }
+//    std::cout << "+++++++++" << std::endl;
+//
+//
+//    std::cout << "********\n" << cfg_ob.get_start_symbol().get_name() << "\n\n";
+//    for (auto non_t : cfg_ob.get_non_terminals())
+//    {
+//        std::cout << non_t.get_name() << std::endl;
+//    }
+//    std::cout << "********" << std::endl;
+//
+//    cfg_ob.print_cfg_symbol_productions_map();
+//    std::shared_ptr<parsing_table> p_table = std::make_shared<parsing_table>(cfg_ob);
+//    cfg_ob.get_first_set()->print_to_console();
+//    cfg_ob.get_follow_set()->print_to_console();
+//
+//    p_table->draw ("parsing_table.txt");
 
-
-    std::cout << "********" << cfg_ob.get_start_symbol().get_name() << std::endl;
-    for (auto non_t : cfg_ob.get_non_terminals())
-    {
-        std::cout << non_t.get_name() << std::endl;
-    }
-    std::cout << "********" << std::endl;
-
-    std::shared_ptr<parsing_table> p_table = std::make_shared<parsing_table>(cfg_ob);
-
-    cfg_ob.print_cfg_symbol_productions_map();
-    cfg_ob.get_first_set()->print_to_console();
-    cfg_ob.get_follow_set()->print_to_console();
-
-    p_table->draw ("parsing_table.txt");
-
-    std::vector<std::string> input_buffer {"int",
-                                           "id",
-                                           ";",
-                                           "if",
-                                           "(",
-                                           "id",
-                                           "relop",
-                                           "num",
-                                           ")",
-                                           "{",
-                                           "id",
-                                           "=",
-                                           "num",
-                                           ";",
-//                                           "}", "else", "{","}",
-                                           "}",
-                                           "$"};
-
-    predictive_parser parser(cfg_ob.get_start_symbol(), p_table, input_buffer);
-    parser.parse();
-
-    std::vector<std::string> stack = parser.get_debug_stack();
-    std::vector<std::string> derivation = parser.get_derivations();
-
-    for (auto s : stack)
-    {
-        std::cout << s << std::endl;
-    }
-
-    std::cout << "***********" << std::endl;
-    std::cout << "***********" << std::endl;
-    std::cout << "***********" << std::endl;
-    std::cout << "***********" << std::endl;
-
-    for (auto d : derivation)
-    {
-        std::cout << d << std::endl;
-    }
+//    std::vector<std::string> input_buffer {"int",
+//                                           "id",
+//                                           ";",
+//                                           "if",
+//                                           "(",
+//                                           "id",
+//                                           "relop",
+//                                           "num",
+//                                           ")",
+//                                           "{",
+//                                           "id",
+//                                           "=",
+//                                           "num",
+//                                           ";",
+////                                           "}", "else", "{","}",
+//                                           "}",
+//                                           "$"};
+//
+//    predictive_parser parser(cfg_ob.get_start_symbol(), p_table, input_buffer);
+//    parser.parse();
+//
+//    std::vector<std::string> stack = parser.get_debug_stack();
+//    std::vector<std::string> derivation = parser.get_derivations();
+//
+//    for (auto s : stack)
+//    {
+//        std::cout << s << std::endl;
+//    }
+//
+//    std::cout << "***********" << std::endl;
+//    std::cout << "***********" << std::endl;
+//    std::cout << "***********" << std::endl;
+//    std::cout << "***********" << std::endl;
+//
+//    for (auto d : derivation)
+//    {
+//        std::cout << d << std::endl;
+//    }
 
 }
