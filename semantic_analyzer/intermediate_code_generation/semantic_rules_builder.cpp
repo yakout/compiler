3e6b994dfe4c1476942edaba8346f4ced5b14397
yakout <iyakout@hotmail.com>
2018-05-05 16:16:37 +0200

    added semantic rules builder file

---
+++ semantic_analyzer/intermediate_code_generation/semantic_rules_builder.cpp
@@ -0,0 +1,11 @@
+#include "semantic_rules.h"
+
+/**
+ * 
+ * this file responsible for building the semantic rules symbols [actions, synthesised attributes]
+ * into the right places in the productions.
+ * 
+ */
+ 
+
+ 
